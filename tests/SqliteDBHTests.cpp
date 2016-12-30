d9f3d134935407d3c565317befade8b660651654
xach <zironside@hotmail.com>
2016-12-29 20:43:54 -0500

    update SqliteDBH tests

--- tests/SqliteDBHTests.cpp
+++ tests/SqliteDBHTests.cpp
@@ -32,55 +32,62 @@ std::string ClientsTable::tableName() {
 	return "clients_table";
 }
 
-TEST_CASE( "Test File Open that does not exist", "[open]" ) {
-	SqliteDBH dbh("mocks/no-file.db");
-	REQUIRE_THROWS(dbh.open());
-};
+TEST_CASE( "Test File Open", "[fileOpen]" ) {
 
-TEST_CASE( "Test File Open", "[open]" ) {
-	SqliteDBH dbh("tests/mocks/test.db");
-	REQUIRE_NOTHROW(dbh.open());
-	dbh.close();
-};
-
-TEST_CASE("Test Create table", "[execute]") {
-	SqliteDBH dbh("tests/mocks/test.db");
-	ClientsTable table;
-	dbh.open();
-	dbh.execute(table.createTableSql());
-	REQUIRE(false);
-	dbh.close();
-};
-
-TEST_CASE("Test Insert", "[execute]") {
-	SqliteDBH dbh("tests/mocks/test.db");
-	ClientsTable table;
-	dbh.open();
-	std::string sql = 	"INSERT INTO clients_table (first_name, last_name, address, phone, email) VALUES ('Tim', 'Dailey', 'Gay Street', '1234567789', 'test@test.com');"\
-						"INSERT INTO clients_table (first_name, last_name, address, phone, email) VALUES ('Tim', 'Dailey', 'Gay Street', '1234567789', 'test@test.com');"\
-						"INSERT INTO clients_table (first_name, last_name, address, phone, email) VALUES ('Tim', 'Dailey', 'Gay Street', '1234567789', 'test@test.com');";
-	std::vector<std::map<std::string,std::string> > rows =  dbh.execute(sql);
-	for(int i = 0; i < rows.size(); i++) {
-		INFO("Row Loop");
-		std::map<std::string, std::string> rowMap;
-		INFO(rowMap["first_name"]);
+	SECTION("Test no file") {
+		SqliteDBH dbh("mocks/no-file.db");
+		REQUIRE_THROWS(dbh.open());
+		dbh.close();
 	}
-	REQUIRE(false);
-	dbh.close();
-}
-
-TEST_CASE("Test Select", "[execute]") {
-	SqliteDBH dbh("tests/mocks/test.db");
-	ClientsTable table;
-	dbh.open();
-	std::string sql = 	"SELECT * FROM clients_table;";
-	std::vector<std::map<std::string,std::string> > rows =  dbh.execute(sql);
-	for(int i = 0; i < rows.size(); i++) {
-		std::map<std::string, std::string> rowMap = rows[i];
-		WARN(rowMap["first_name"]);
+	SECTION("Test File Exists") {
+		SqliteDBH dbh("tests/mocks/test.db");
+		REQUIRE_NOTHROW(dbh.open());
+		dbh.close();
 	}
-	INFO(rows.size());
-	dbh.execute("DROP TABLE clients_table");
-	REQUIRE(false);
-	dbh.close();
+};
+
+ClientsTable table;
+TEST_CASE("Test sql functions", "[execute]") {
+	struct TestSetup {
+		static SqliteDBH setupDBH() {
+			SqliteDBH dbh("tests/mocks/test.db");
+			dbh.open();
+			return dbh;
+		}
+	};
+	SECTION("Test Create db") {
+		SqliteDBH dbh = TestSetup::setupDBH();
+		dbh.execute(table.createTableSql());
+		REQUIRE(false);
+		dbh.close();
+	}
+
+	SECTION("Test Insert") {
+		SqliteDBH dbh = TestSetup::setupDBH();
+		std::string sql = 	"INSERT INTO clients_table (first_name, last_name, address, phone, email) VALUES ('Tim', 'Dailey', 'Gay Street', '1234567789', 'test@test.com');"\
+							 "INSERT INTO clients_table (first_name, last_name, address, phone, email) VALUES ('Tim', 'Dailey', 'Gay Street', '1234567789', 'test@test.com');"\
+							 "INSERT INTO clients_table (first_name, last_name, address, phone, email) VALUES ('Tim', 'Dailey', 'Gay Street', '1234567789', 'test@test.com');";
+		std::vector<std::map<std::string,std::string> > rows =  dbh.execute(sql);
+		for(int i = 0; i < rows.size(); i++) {
+			INFO("Row Loop");
+			std::map<std::string, std::string> rowMap;
+			INFO(rowMap["first_name"]);
+		}
+		REQUIRE(false);
+		dbh.close();
+	}
+
+	SECTION("Test Select") {
+		SqliteDBH dbh = TestSetup::setupDBH();
+		std::string sql = 	"SELECT * FROM clients_table;";
+		std::vector<std::map<std::string,std::string> > rows =  dbh.execute(sql);
+		for(int i = 0; i < rows.size(); i++) {
+			std::map<std::string, std::string> rowMap = rows[i];
+			WARN(rowMap["first_name"]);
+		}
+		INFO(rows.size());
+		dbh.execute("DROP TABLE clients_table");
+		REQUIRE(false);
+		dbh.close();
+	}
 }
